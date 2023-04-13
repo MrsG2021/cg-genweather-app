0b4477f3e487b95691bd008034e266da82c67f8e
MrsG2021 <caryglanz@outlook.com>
2023-04-13 12:16:32 +0100

    changed the content of the page

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <h1>Weather app</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <footer>
+          This project was coded by Cary Glanzmann and is {""}
+          <a
+            className="App-link"
+            href="https://github.com/MrsG2021/cg-genweather-app.git"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
       </header>
     </div>
   );
