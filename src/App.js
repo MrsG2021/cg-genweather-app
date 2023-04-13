8c1b93539dae606dcae1d29d4f53f2744a047500
MrsG2021 <caryglanz@outlook.com>
2023-04-13 12:07:20 +0100

    Removed unwanted files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather app</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
