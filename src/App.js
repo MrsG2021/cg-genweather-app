1a4c7d153bcb6848c2c91ade69b29c98bd2a3fab
MrsG2021 <caryglanz@outlook.com>
2023-04-13 12:49:56 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather app</h1>
+      <div className="container">
+        <h1>Weather app!</h1>
+        <button className="btn btn-primary">Hello</button>
         <footer>
           This project was coded by Cary Glanzmann and is {""}
           <a
             className="App-link"
             href="https://github.com/MrsG2021/cg-genweather-app.git"
             target="_blank"
+            without
+            rel="noreferrer"
           >
             open-sourced on Github
           </a>
         </footer>
-      </header>
+      </div>
     </div>
   );
 }
