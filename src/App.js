2916614d66f046404fcb5551f56bbddeae0a052d
MrsG2021 <caryglanz@outlook.com>
2023-05-06 16:18:47 +0100

    Added basic HTML structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Reatct from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather app!</h1>
-        <button className="btn btn-primary">Hello</button>
+        <Weather />
+
         <footer>
           This project was coded by Cary Glanzmann and is {""}
           <a
